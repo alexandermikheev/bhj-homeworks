840e710ac45904e8020f8e22ced96772d737e23a
Александр Михеев <nyafkin2012@yandex.ru>
2023-01-29 01:14:34 +0300

    Исправление таймера

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,23 +1,42 @@
-const p = document.getElementById('timer');
-let arrStr = p.textContent.split(':');
-let arr = arrStr.map(item=> Number(item));
-let sec = arr[0]*3600 + arr[1]*60 + arr[2]
-let zero = (str) => {if (str.length == 1){return '0'+str} else{return str}};
+// let timer; 
+// let x = Number(document.getElementById('timer').textContent);
+// countdown(); 
+// function countdown(){  
+//   document.getElementById('timer').innerHTML = x;
+//   x--; 
+//   if (x<0){
+//     clearTimeout(timer); 
+//     document.getElementById('timer').innerHTML = "Вы победили в конкурсе!"
+//     alert(`Вы победили в конкурсе!`)
+//   }
+//   else {
+//     timer = setTimeout(countdown, 1000);
+//   }
+// }
+// Задание #0
 
-function ret(a){
-  let hh = Math.floor(a/3600);
-  let mm = Math.floor((a-hh*3600)/60);
-  let ss = a - hh*3600 - mm*60;
-  return zero(String(hh))+':'+zero(String(mm))+':'+zero(String(ss));
+let timer = document.getElementById('timer');
+
+setInterval(timerupdate,1000)
+
+clock = 5
+
+function timerupdate(){
+  let hours = clock > 0 ? Math.floor(clock / 60 / 60) % 24 : 0;
+  let minutes = clock > 0 ? Math.floor(clock  / 60) % 60 : 0;
+  let seconds = clock > 0 ? clock  % 60 : 0;
+  seconds = seconds < 10 ? "0" + seconds: seconds;
+  minutes = minutes < 10 ? "0" + minutes: minutes;
+  hours = hours < 10 ? "0" + hours: hours;
+  timer.innerHTML =`${hours}:${minutes}:${seconds}`;
+  clock--
+
+  if(clock < 0){
+    var oldLocation = location;
+    location = "https://static-forum.gipsyteam.ru/post-88160-1489909878_thumb.jpg";
+  }
+
 }
 
-let timerId = setInterval(() => {
-  p.textContent = ret(sec - 1);
-  sec -= 1;
-  if (sec == 0) {
-    clearInterval(timerId);
-    location ='https://aldebaran.ru/author/evgenevich_borisov_konstantin/kniga_kak_horoshemu_razrabotchiku_ne_stat_ploh/download.fb2.zip';
-
-    //alert("You are win!");
-    }
-  }, 1000);
+//  Я конечно мог вставить зипник со своего серва с этой картинкой,
+//  но это задание меня выбесило, прошу заценить мое отчание
