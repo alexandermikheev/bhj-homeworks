58d8f33aafc10ea67896ab33c326e0bb83d66ca0
Александр Михеев <nyafkin2012@yandex.ru>
2023-01-29 02:03:12 +0300

    допил 2-ое задание

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,6 +1,15 @@
 const element = document.getElementById('cookie');
 const screenCount = document.getElementById('clicker__counter');
-const average = document.getElementById('average__counter');
+
+let average = document.createElement('div');
+average.id ="average__counter"
+average.setAttribute("style", "margin-top: 30px");
+
+let clickerStatus = document.getElementById('clicker__status');
+
+clickerStatus.append(average)
+
+
 let count = Number(element.textContent);
 let data1 = new Date();
 let sum = 0;
@@ -8,8 +17,8 @@ element.onclick = function() {
   let data = new Date();
   count ++;
   screenCount.textContent = count;
-  if (count%2 == 0) {element.width = 200} else {element.width = 180};
+  if (count %2 == 0) {element.width = 200} else {element.width = 180};
   sum += (data - data1)/1000 
-  average.textContent = (sum/count).toFixed(2);
+  average.textContent = `Скорость клика ${(sum/count).toFixed(2)}`;
   data1 = data;
 };
